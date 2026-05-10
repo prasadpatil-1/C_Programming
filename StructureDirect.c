1c46fbe9b5b1c5296b77073e8dc3b99e25d9b28e
Prasad Patil <pgpatil614@gmail.com>
2026-05-10 21:45:12 +0530

    Add C program for Structure Direct declaration

---
+++ StructureDirect.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+// Declaration
+#pragma pack(1)
+struct Demo
+{
+    int i;      //4
+    char ch;    //1     Genarate Padding
+    float f;    //4
+};              //8
+
+int main()
+{
+    struct Demo dobj;
+
+    dobj.i = 11;
+    dobj.ch = 'A';
+    dobj.f = 10.0f;
+
+    printf("%d\n",dobj.i); //11
+    printf("%c\n",dobj.ch);//A
+    printf("%f\n",dobj.f);//10.0
+
+    return 0;
+
+}
