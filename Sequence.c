11619180f69741d4063c8df7f0d67d957557fe22
Prasad Patil <pgpatil614@gmail.com>
2026-05-26 16:17:42 +0530

    sequence

---
+++ Sequence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0, Ans = 0;
+    
+    printf("Enter first number : \n",No1);
+    scanf("%d\n");
+    printf("Enter Second number : \n",No2);
+    scanf("%d\n");
+
+    Ans = No1 + No2;
+    printf("Addition is : %d\n",Ans);
+
+    return 0;
+
+}
