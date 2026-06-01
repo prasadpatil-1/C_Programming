012080de771c9b740889b87dfb86a4bd65fdb235
Prasad Patil <pgpatil614@gmail.com>
2026-06-01 22:28:49 +0530

    Multi file project Demonstration in C Prgramming

---
+++ Mathematics_Project/Helper.c
@@ -0,0 +1,11 @@
+// Function Defination
+
+int Addition(int A ,int B)
+{
+    return A+B;
+}
+
+int Substraction(int A ,int B)
+{
+    return A-B;
+}
---
+++ Mathematics_Project/Marvellous.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+// Function Protype or Function Declaration
+int Addition(int , int);
+
+int Substraction(int , int);
